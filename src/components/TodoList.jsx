43e788bf55fe1765dc681a0664880c7c02db6dc8
Duic <ducvvm4a@gmail.com>
2025-05-27 23:34:39 +0700

    sorted function

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,13 @@
 import ToDoItem from "./ToDoItem";
 
 export default function TodoList({ todos, setTodos }) {
-  if (!todos || todos.length === 0) return null;
 
+  if (!todos || todos.length === 0) return null;
+  const sortedTodos = todos.slice().sort((a,b) => Number(a.done)-Number(b.done))
   return (
     <div className="flex justify-center mt-7 ">
       <div className="border border-amber-700 flex flex-col items-center w-full max-w-4xl  pb-2.5 rounded-4xl shadow-lg shadow-blue-400">
-        {todos.map((item) => (
+        {sortedTodos.map((item) => (
           <ToDoItem key={item.name} item={item} todos={todos} setTodos={setTodos} />
         ))}
       </div>
